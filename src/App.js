9898b16303dc4a349d7646d52c0d62a776ff473a
satishngupta27 <55748040+satishngupta27@users.noreply.github.com>
2020-08-21 07:13:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
 <<<<<<< HEAD
     <div><h1>This is our mini project <br/> Coaching Management Web App btech cse satish gupta sushant</h1></div>
 
-=======
-    <div><h1>This is our mini project <br/> Coaching Management Web App btech cse satish gupta</h1></div>
->>>>>>> a4f90266db3f18f3cb476121e365b150451c4d85
+
   );
 }
 
