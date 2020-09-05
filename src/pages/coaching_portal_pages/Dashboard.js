cec34d0a2bcfb4d7a97724dacf16d3266f50f043
SATISH <satish.gupta_cs18@gla.ac.in>
2020-09-05 19:10:36 -0400

    dashboard element added

--- src/pages/coaching_portal_pages/Dashboard.js
+++ src/pages/coaching_portal_pages/Dashboard.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {Container , Row } from 'react-bootstrap';
+import Overview_Card from '../../components/Overiew_Card';
+import DateAndTime from '../../components/Date-And-Time';
+import GreetingUser from '../../components/GreetingUser';
+
 
 function Dashboard() {
   return (
-    <div className='dashboard'>
-      <h1>Dashboard</h1>
-    </div>
+    <>
+    <Container >
+      <Row style={{justifyContent:'Space-between',textAlign:'right'}}>
+        <GreetingUser/>
+        <DateAndTime/>
+      </Row>
+      <Row style={{margin:'auto' , justifyContent:'center'}}>
+        <Overview_Card title={'Total Student'} value={'560'}/>
+        <Overview_Card title={'Braches'} value={'2'}/>
+        <Overview_Card title={'Batches'} value={'6'}/>
+        <Overview_Card title={'Teachers'} value={'10'}/>
+        <Overview_Card title={'Subjects'} value={'5'}/>
+      </Row>
+    </Container>
+    </>
   );
 }
 
