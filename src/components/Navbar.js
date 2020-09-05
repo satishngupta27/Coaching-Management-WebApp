036317804b4f40678f4838177542987581d09605
SATISH <satish.gupta_cs18@gla.ac.in>
2020-09-05 12:20:38 -0400

    navbar minimize added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,10 +20,10 @@ function Navbar() {
           </Link>
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul className='nav-menu-items' onClick={showSidebar}>
+          <ul className='nav-menu-items' >
             <li className='navbar-toggle'>
               <Link to='#' className='menu-bars'>
-                <AiIcons.AiOutlineClose />
+                <AiIcons.AiOutlineClose onClick={showSidebar} />
               </Link>
             </li>
             {SidebarData.map((item, index) => {
