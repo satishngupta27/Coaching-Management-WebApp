70017994f9b52b7c35b1777ce841f55e23b8a76e
SATISH <satish.gupta_cs18@gla.ac.in>
2020-10-13 11:17:32 -0400

    routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Academics from './pages/coaching_portal_pages/Academics';
+import Academics from './pages/coaching_portal_pages/Academics/Academics';
 import Dashboard from './pages/coaching_portal_pages/Dashboard';
-import Student_Info from './pages/coaching_portal_pages/Student_Info';
+import Student_Info from './pages/coaching_portal_pages/Student-info/Student_Info';
+import AddStudent from './pages/coaching_portal_pages/Student-info/AddStudent';
+import ViewAllStudent from './pages/coaching_portal_pages/Student-info/ViewAllStudent';
+import deleteStudent from './pages/coaching_portal_pages/Student-info/deleteStudent';
+import UpdateStudent from './pages/coaching_portal_pages/Student-info/UpdateStudent';
+
+
+
 
 
 
@@ -16,7 +23,11 @@ function App() {
         <Switch>
           <Route path='/' exact component={Dashboard} />
           <Route path='/academics' component={Academics} />
-          <Route path='/student_Info' component={Student_Info} />
+          <Route path='/student_Info' exact component={Student_Info} />
+          <Route path='/student_Info/addStudent' exact component={AddStudent} />
+          <Route path='/student_Info/viewAllStudent' exact component={ViewAllStudent} />
+          <Route path='/student_Info/deleteStudent' exact component={deleteStudent} />
+          <Route path='/student_Info/updateStudent' exact component={UpdateStudent} />
         </Switch>
       </Router>
   );
