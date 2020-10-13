da89120c87c7db8572dcaa1aa1381cd0c67701de
SATISH <satish.gupta_cs18@gla.ac.in>
2020-10-13 11:13:51 -0400

    user greeting

--- frontend/src/components/GreetingUser.js
+++ frontend/src/components/GreetingUser.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import { Card, Container, Col } from 'react-bootstrap';
-
+import React from "react";
+import {  Col } from "react-bootstrap";
 
 let currentHour = new Date().getHours();
-let greeting = '';
+let greeting = "";
 
-if (currentHour>1 && currentHour<12){
-    greeting='Good Morning';
-
-}else if(currentHour>=12 && currentHour<19){
-    greeting='Good Afternoon'
-}else{
-    greeting ='Good Night';
+if (currentHour > 1 && currentHour < 12) {
+  greeting = "Good Morning";
+} else if (currentHour >= 12 && currentHour < 19) {
+  greeting = "Good Afternoon";
+} else {
+  greeting = "Good Night";
 }
 function GreetingUser() {
   return (
-      <>
-      <Col style={{textAlign:'left'}}>
-  <h2>{greeting}</h2>
-      
-      <h4>Harry</h4>
-        </Col>
-      
-    
-  </>
+    <>
+      <Col style={{ textAlign: "left" }}>
+        <h2>{greeting}</h2>
+
+        <h4>Harry</h4>
+      </Col>
+    </>
   );
 }
 
