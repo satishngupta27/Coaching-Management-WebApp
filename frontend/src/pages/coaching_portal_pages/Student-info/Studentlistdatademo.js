1e6078a8b9c0dcb3cc214f107cc0fb9d740f77cb
SATISH <satish.gupta_cs18@gla.ac.in>
2020-10-14 13:46:07 -0400

    demo data studentinfo

---
+++ frontend/src/pages/coaching_portal_pages/Student-info/Studentlistdatademo.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+
+import * as IoIcons from 'react-icons/io';
+
+export const StudentlistDataDemo = [
+  {
+    rollno:'1',
+    firstName: "sg",
+    lastName: "la",
+    email: "esskjk",
+    mobileNumber: "dfs24345",
+    selectBrachOption: "5424",
+    selectBatchOption: "3543",
+    GenderRadioOption: "24524",
+    guardianName:'hsgh',
+    guadianMobileNumber:'sds',
+    address:'sgsfg',
+    state:'sgs',
+    city:"sfsf",
+    icon: <IoIcons.IoMdHelpCircle />,
+    birthDate: null,
+
+  },
+  {
+    rollno:'2',
+    firstName: "sg",
+    lastName: "la",
+    email: "esskjk",
+    mobileNumber: "dfs24345",
+    selectBrachOption: "5424",
+    selectBatchOption: "3543",
+    GenderRadioOption: "24524",
+    guardianName:'hsgh',
+    guadianMobileNumber:'sds',
+    address:'sgsfg',
+    state:'sgs',
+    city:"sfsf",
+    icon: <IoIcons.IoMdHelpCircle />,
+    birthDate: null,
+
+  },
+
+  
+];
