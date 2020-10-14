b1c00d0436d795151f6fb08db35d699ae7bd24c2
SATISH <satish.gupta_cs18@gla.ac.in>
2020-10-14 13:45:45 -0400

    profile button added in table

--- frontend/src/pages/coaching_portal_pages/Student-info/Student_Info.js
+++ frontend/src/pages/coaching_portal_pages/Student-info/Student_Info.js
@@ -1,41 +1,68 @@
 import React from "react";
 import MenuButton from "../../../components/MenuButton";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { MdDelete, MdCreate, MdPerson } from "react-icons/md";
+import { StudentlistDataDemo } from "./Studentlistdatademo";
 
 function Student_Info() {
   return (
     <div className="student_info">
       <h1 style={{ textAlign: "center" }}>Student_Info</h1>
       <Container>
-        <Row style={{ justifyContent: "flex-end", textAlign: "center" }}>
+        <Row
+          style={{
+            justifyContent: "flex-end",
+            textAlign: "center",
+            marginBottom: "20px",
+          }}
+        >
           <Link to="./Student_Info/addStudent">
             {/* {item.icon} */}
             <span>
               <MenuButton title={"Add Student"} />
             </span>
           </Link>
-          <Link to="./Student_Info/deletetudent">
-            {/* {item.icon} */}
-            <span>
-              <MenuButton title={"Delete Student"} />
-            </span>
-          </Link>
-          <Link to="./Student_Info/updateStudent">
-            {/* {item.icon} */}
-            <span>
-              <MenuButton title={"Update Student Info"} />
-            </span>
-          </Link>
-          <Link to="./Student_Info/viewAllStudent">
-            {/* {item.icon} */}
-            <span>
-              <MenuButton title={"View Student Info"} />
-            </span>
-          </Link>
-
-          
         </Row>
+        <h1>List of Student</h1>
+
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+              <th>Mobile No.</th>
+              <th>Guardian Name</th>
+              <th>Operation</th>
+            </tr>
+          </thead>
+          <tbody>
+            {StudentlistDataDemo.map((item, index) => {
+              return (
+                <tr>
+                  <td>{item.rollno}</td> 
+                  <td>{item.firstName}</td>
+                  <td>{item.lastName}</td>
+                  <td>{item.email}</td>
+                  <td>{item.mobileNumber}</td>
+                  <td>{item.guardianName}</td>
+                  <td style={{ textAlign: "center" }}>
+                    <MdDelete />
+                    <MdCreate />
+                    <Link to={`./Student_Info/studentprofile/${item.firstName}`} >
+                      <span>
+                        <MdPerson />
+                      </span>
+                    </Link>
+                    {/* <Link to={`/student_Info/studentprofile/+this.props.rollno}`}>More info</Link> */}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
       </Container>
     </div>
   );
