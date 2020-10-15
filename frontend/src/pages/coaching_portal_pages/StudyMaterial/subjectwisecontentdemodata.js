e82861c747de8146d39ac7b8ddcf0c43c17f6f90
SATISH <satish.gupta_cs18@gla.ac.in>
2020-10-15 12:57:53 -0400

    subject dummy data

---
+++ frontend/src/pages/coaching_portal_pages/StudyMaterial/subjectwisecontentdemodata.js
@@ -0,0 +1,28 @@
+
+
+export const Subjectwisecontentdemodata = [
+  {
+    title: "English",
+    path: "./studyMaterialType",
+    // imgUrl:logo,
+    subtitle: "this content",
+  },
+  {
+    title: "Mathematics",
+    path: "/",
+    // imgUrl:logo,
+    subtitle: "this content",
+  },
+  {
+    title: "Physics",
+    path: "/",
+    // imgUrl:logo,
+    subtitle: "this content",
+  },
+  {
+    title: "Chemistry",
+    path: "/",
+    // imgUrl:logo,
+    subtitle: "this content",
+  },
+];
