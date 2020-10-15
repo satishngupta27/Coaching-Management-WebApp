1634199f1372c25d65bbaefa76f6f83091f667f5
SATISH <satish.gupta_cs18@gla.ac.in>
2020-10-15 12:54:10 -0400

    Added more routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Academics from './pages/coaching_portal_pages/Academics/Academics';
+import StudyMaterialType from './pages/coaching_portal_pages/StudyMaterial/studyMaterialType';
+import SubjectWise from './pages/coaching_portal_pages/StudyMaterial/SubjectWise';
+
+import Assignment from './pages/coaching_portal_pages/StudyMaterial/Assignment';
+import CreateNewsSubjectForm from './pages/coaching_portal_pages/StudyMaterial/CreateNewSubjectForm';
+import UploadNotesForm from './pages/coaching_portal_pages/StudyMaterial/UploadNotesForm';
+
 import Dashboard from './pages/coaching_portal_pages/Dashboard';
 import Student_Info from './pages/coaching_portal_pages/Student-info/Student_Info';
 import AddStudent from './pages/coaching_portal_pages/Student-info/AddStudent';
 import ViewAllStudent from './pages/coaching_portal_pages/Student-info/ViewAllStudent';
 import UpdateStudent from './pages/coaching_portal_pages/Student-info/UpdateStudent';
 import StudentProfile from './pages/coaching_portal_pages/Student-info/StudentProfile';
+import Notes from './pages/coaching_portal_pages/StudyMaterial/Notes';
+
 
 
 function App() {
@@ -18,12 +26,18 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Dashboard} />
-          <Route path='/academics' component={Academics} />
+          <Route path='/studymaterialType' component={StudyMaterialType} />
+          <Route path='/createNewSubjectForm' component={CreateNewsSubjectForm} />
+          <Route path='/uploadNotesForm' component={UploadNotesForm} />
+          <Route path='/subjectwise' component={SubjectWise} />
+          <Route path='/notes' component={Notes} />
+          <Route path='/assignment' component={Assignment} />
           <Route path='/student_Info' exact component={Student_Info} />
           <Route path='/student_Info/addStudent' exact component={AddStudent} />
           <Route path='/student_Info/viewAllStudent' exact component={ViewAllStudent} />
           <Route path='/student_Info/updateStudent' exact component={UpdateStudent} />
           <Route path='/student_Info/studentprofile/:id' exact component={StudentProfile} />
+          
           
         </Switch>
       </Router>
