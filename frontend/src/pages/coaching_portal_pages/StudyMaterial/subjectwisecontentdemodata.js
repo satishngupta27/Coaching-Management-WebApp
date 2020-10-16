d18b62dd4ca8b2291b1af491f00cb8b32d456159
SATISH <satish.gupta_cs18@gla.ac.in>
2020-10-16 16:01:01 -0400

    subject wise demo json

--- frontend/src/pages/coaching_portal_pages/StudyMaterial/subjectwisecontentdemodata.js
+++ frontend/src/pages/coaching_portal_pages/StudyMaterial/subjectwisecontentdemodata.js
@@ -4,25 +4,25 @@ export const Subjectwisecontentdemodata = [
   {
     title: "English",
     path: "./studyMaterialType",
-    // imgUrl:logo,
-    subtitle: "this content",
+    imgUrl:"https://images.vexels.com/media/users/3/169938/preview2/f02b599d55c05825554ded12f0e84af0-english-subject-illustration.jpg",
+    subtitle: " Enlgish content in this folder",
   },
   {
     title: "Mathematics",
-    path: "/",
-    // imgUrl:logo,
+    path: "./studyMaterialType",
+    imgUrl:"https://i.pinimg.com/736x/59/d7/87/59d7878792e5698cf4e100d429dc952e.jpg",
     subtitle: "this content",
   },
   {
     title: "Physics",
-    path: "/",
-    // imgUrl:logo,
+    path: "./studyMaterialType",
+    imgUrl:"https://kit8.net/images/thumbnails/580/435/detailed/3/physicist@2x.png",
     subtitle: "this content",
   },
   {
     title: "Chemistry",
-    path: "/",
-    // imgUrl:logo,
+    path: "./studyMaterialType",
+    imgUrl:"https://i.pinimg.com/736x/87/aa/03/87aa0323e9e195c0ae3dc6c69a8aeb74.jpg",
     subtitle: "this content",
   },
 ];
