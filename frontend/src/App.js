e47f2965d19ad238868218cbdd723cf4d298de6b
YashSingh009 <yashaswi.singh_cs18@gla.ac.in>
2020-10-18 14:02:05 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Academics from './pages/coaching_portal_pages/Academics/Academics';
+// import Academics from './pages/coaching_portal_pages/Academics/Academics';
 import Dashboard from './pages/coaching_portal_pages/Dashboard';
 import Student_Info from './pages/coaching_portal_pages/Student-info/Student_Info';
-import AddStudent from './pages/coaching_portal_pages/Student-info/AddStudent';
+// import AddStudent from './pages/coaching_portal_pages/Student-info/AddStudent';
 import ViewAllStudent from './pages/coaching_portal_pages/Student-info/ViewAllStudent';
-import deleteStudent from './pages/coaching_portal_pages/Student-info/deleteStudent';
+// import deleteStudent from './pages/coaching_portal_pages/Student-info/deleteStudent';
 import UpdateStudent from './pages/coaching_portal_pages/Student-info/UpdateStudent';
 
 
@@ -22,11 +22,11 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Dashboard} />
-          <Route path='/academics' component={Academics} />
+          {/* <Route path='/academics' component={Academics} /> */}
           <Route path='/student_Info' exact component={Student_Info} />
-          <Route path='/student_Info/addStudent' exact component={AddStudent} />
+          {/* <Route path='/student_Info/addStudent' exact component={AddStudent} /> */}
           <Route path='/student_Info/viewAllStudent' exact component={ViewAllStudent} />
-          <Route path='/student_Info/deleteStudent' exact component={deleteStudent} />
+          {/* <Route path='/student_Info/deleteStudent' exact component={deleteStudent} /> */}
           <Route path='/student_Info/updateStudent' exact component={UpdateStudent} />
         </Switch>
       </Router>
