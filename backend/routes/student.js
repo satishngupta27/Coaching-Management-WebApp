28d9bf6326f44b4834e04511804af0a0b6687c9d
SATISH <satish.gupta_cs18@gla.ac.in>
2020-11-07 02:39:07 -0500

    backend route created

---
+++ backend/routes/student.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const Student = require("../models/student");
+
+router.route("/addStudent").post(async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    mobileNumber,
+    batch,
+    gender,
+    dob,
+    guardianName,
+    gurardianMobileNumber,
+    address,
+    state,
+    city,
+  } = req.body;
+
+  const newStudent = new Student({
+    firstName,
+    lastName,
+    email,
+    mobileNumber,
+    batch,
+    gender,
+    dob,
+    guardianName,
+    gurardianMobileNumber,
+    address,
+    state,
+    city,
+  });
+  await newStudent.save();
+});
+
+router.route("/students").get((req, res) => {
+  Student.find().then((students) => res.json(students));
+});
+
+module.exports = router;
