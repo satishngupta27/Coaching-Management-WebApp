ac481fc3a274ec76822722183b658db3d48cb8ab
SATISH <satish.gupta_cs18@gla.ac.in>
2020-11-07 02:40:54 -0500

    router divided

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import "./App.css";
 import PageRoutes from './PageRoutes';
 import Login from './login';
 
 function App() {
-  const [isLoggedIn,setIsLoggedIn]= useState(true);
-  const [isStudent,setIsStudent]= useState(true);
+  const [isLoggedIn,setIsLoggedIn]= useState(false);
+  const [isStudent,setIsStudent]= useState(false);
+
+  
+  
+  const handleIsLogin= (e)=>{
+    setIsLoggedIn(!isLoggedIn)
+    
+  }
+  const handleIsStudent=(e)=>{
+    setIsStudent(!isStudent)
+    
+  }
   
   if(!isLoggedIn){
-    return <Login isStudent={isStudent}/>  
+    return <Login handleIsLogin={handleIsLogin} handleIsStudent={handleIsStudent} isStudentState={isStudent}/>  
   }
   else {return <PageRoutes isStudent={isStudent}/>}  
 }
