f18b5efb1f256decb0692468ba4b853919f88550
SATISH <satish.gupta_cs18@gla.ac.in>
2020-11-10 04:19:04 -0500

    update imports

--- frontend/src/pages/Student_portal_pages/MyDoubt/MyDoubt.js
+++ frontend/src/pages/Student_portal_pages/MyDoubt/MyDoubt.js
@@ -1,6 +1,8 @@
 import  React,{useState,useEffect} from 'react';
-
+import { Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import MenuButton from "../../../components/MenuButton";
+import AssignmentCard from "../../../components/AssignmentCard";
 import DoubtCard from "../../../components/DoubtCard";
 
 
@@ -21,28 +23,34 @@ function MyDoubt() {
     }).then(jsonRes=>setDoubts(jsonRes));
   },[])
     return (
-      <>
-    
-      
-      
+        <div>
+        <h1 style={{ textAlign: "center" }}>Doubt</h1>
+        <Container>
+          <Row style={{ justifyContent: "flex-end", textAlign: "center" }}>
+            <Link to="./myDoubt/askDoubt">
+              <MenuButton title={"Ask Doubt"} />
+            </Link>
+          </Row>
+          <Row>
+            <h1>All doubt </h1>
+          </Row>
+          {doubts.map((item, index) => {
 
-      <h1 style ={{color:"red",textAlign:"center"}}> My Doubts</h1>
-      {doubts.map((item, index) => {
+return ( 
+  <div className="container">
+<DoubtCard names = {item.postedBy} question = {item.question} answer={item.answer} /> 
+<Link to={`./studentDoubt/${item._id}`}>
+<div style={{marginLeft: "1021px"}}></div>
+  </Link> 
+</div>
 
-            return ( 
-              <div className="container">
-            <DoubtCard names = {item.postedBy} question = {item.question} answer={item.answer} /> 
-            <Link to={`./studentDoubt/${item._id}`}>
-            
-              </Link> 
-            </div>
 
-            
-          )
-          } 
-      )
-      } 
-  </>
+)
+} 
+)
+} 
+        </Container>
+        </div>
     )
 }
 
