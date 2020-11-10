1f1db37577230ba573380d7bb8f5acaf86ef6142
SATISH <satish.gupta_cs18@gla.ac.in>
2020-11-10 04:00:01 -0500

    delete unwanted imports

--- frontend/src/pages/coaching_portal_pages/StudentDoubt/Student_Doubt.js
+++ frontend/src/pages/coaching_portal_pages/StudentDoubt/Student_Doubt.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import {Container , Row } from 'react-bootstrap';
-import DateAndTime from '../../../components/Date-And-Time';
-import { Formik, Form } from "formik";
-import * as Yup from "yup";
-import FormikControl from "../../../components/FormComponents/FormikControl";
+import React,{useState,useEffect} from 'react';
 import { Link } from "react-router-dom";
-import { studentDoubtDemoData } from "./studentDoubtDemoData";
 import DoubtCard from "../../../components/DoubtCard";
 import MenuButton from '../../../components/MenuButton';
 function Student_Doubt() {
+  const [doubts,setDoubts]=useState([{
+    _id:"",
+    question:'',
+    answer:"",
+    postedBy:""
+   
+  }])
+
+  useEffect(()=>{
+     fetch("/doubts").then(res=>{
+      if(res.ok){
+        return res.json()
+      }
+    }).then(jsonRes=>setDoubts(jsonRes));
+  },[])
+
 
 return(
     <>
@@ -17,12 +27,12 @@ return(
       
 
         <h1 style ={{color:"red",textAlign:"center"}}> Questions</h1>
-        {studentDoubtDemoData.map((item, index) => {
+        {doubts.map((item, index) => {
 
               return ( 
                 <div className="container">
-              <DoubtCard names = {item.names} question = {item.question} answer={item.answer} /> 
-              <Link to={`./studentDoubt/${item.id}`}>
+              <DoubtCard names = {item.postedBy} question = {item.question} answer={item.answer} /> 
+              <Link to={`./studentDoubt/${item._id}`}>
               <div style={{marginLeft: "1021px"}}><MenuButton title={"Answer"} /></div>
                 </Link> 
               </div>
