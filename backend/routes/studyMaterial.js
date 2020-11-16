b4acedab024d4e2645cb6be33f6f86c4395ce8d1
SATISH <satish.gupta_cs18@gla.ac.in>
2020-11-16 00:23:17 -0500

    added upload routes

--- backend/routes/studyMaterial.js
+++ backend/routes/studyMaterial.js
@@ -1,10 +1,10 @@
 const StudyMaterial = require("../models/studyMaterial");
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const { v4: uuidv4 } = require("uuid");
 const AWS = require("aws-sdk");
-
+const fs = require("fs");
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -12,10 +12,57 @@ const s3 = new AWS.S3({
   region: process.env.AWS_REGION,
 });
 
-router.route("/studyMaterials").post((req, res) => {
-   
-       console.log(req.files)
-    
+router.route("/studyMaterials").post(async (req, res) => {
+  // console.log("body",req.body)
+  //const image= new Buffer(req.body)
+  console.log("file", req.files.file);
+  const Blob = req.files.file.data;
+  const content = req.files.file;
+
+  console.log(req.body);
+  const {
+    ChapterName,
+    TopicName,
+    WrittenBy,
+    notesTypedropdownOptions,
+  } = req.body;
+  let studyMaterial = new StudyMaterial({
+    ChapterName: ChapterName,
+    TopicName: TopicName,
+    WrittenBy: WrittenBy,
+    ContentType: notesTypedropdownOptions,
+  });
+  //upload image to s3
+  const params = {
+    Bucket: "mystudynotesbucket",
+    Key: `${req.files.file.name}`,
+    Body: fs.readFileSync(req.files.file.tempFilePath),
+    ACL: "public-read",
+    ContentType: `application/pdf`,
+  };
+
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.log("upload fail", err);
+      return res.status(400).json({ error: "upload to s3 failed" });
+    }
+    console.log("AWS UPLOAD RES DATA", data);
+    studyMaterial.content.url = data.Location;
+    studyMaterial.content.key = data.Key;
+
+    //save to db
+
+    studyMaterial.save((err, success) => {
+      if (err) {
+        return res.status(400).json({ error: "ERROR saving category to db" });
+      }
+      return res.json(success);
+    });
+  });
 });
 
-module.exports= router;
+router.route("/myStudyMaterial").get((req, res) => {
+  StudyMaterial.find().then((notes) => res.json(notes));
+});
+
+module.exports = router;
