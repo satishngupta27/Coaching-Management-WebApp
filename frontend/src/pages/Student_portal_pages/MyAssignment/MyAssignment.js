93c113f24e702fb720b023d941cea7568de32f9d
SATISH <satish.gupta_cs18@gla.ac.in>
2020-11-18 00:01:21 -0500

    fetch submit assignment

--- frontend/src/pages/Student_portal_pages/MyAssignment/MyAssignment.js
+++ frontend/src/pages/Student_portal_pages/MyAssignment/MyAssignment.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Container, Row } from "react-bootstrap";
 import MenuButton from '../../../components/MenuButton';
 import { Link } from "react-router-dom";
@@ -7,29 +7,44 @@ import AssignmentCard from "../../../components/AssignmentCard";
 import {AssignmentDemoData} from '../../coaching_portal_pages/Assignment/AssignmentDemoData';
 
 function MyAssignment() {
-    return (
-        <div>
-            <h1 style={{ textAlign: "center" }}>Assignment</h1>
-      <Container>
-       
-        {AssignmentDemoData.map((item, index) => {
-          return ( 
-            <div className="container">
-           
-          <Link to={`./myAssignment/${item.id}`} style={{textDecoration:'none'}}>
-          <AssignmentCard title={item.title} dueDate={item.dueDate}/>
-          
-            </Link> 
-          </div>
+  const [assignments,setAssignment]=useState([{
+    _id:"",
+    title: "",
+    subject: "",
+    point: "",
+    dueDate: null,
+    
+   
+  }])
 
-          
-        )
-          
-         
+  useEffect(()=>{
+     fetch("/assignment").then(res=>{
+      if(res.ok){
+        return res.json()
+      }
+    }).then(jsonRes=>setAssignment(jsonRes));
+  },[])
+    return (
+      <div>
+      <h1 style={{ textAlign: "center" }}>Assignment</h1>
+      <Container>
+        <Row style={{ justifyContent: "flex-end", textAlign: "center" }}>
+          <Link to="./createAssignmentForm">
+            <MenuButton title={"Create Assignment"} />
+          </Link>
+        </Row>
+        <Row>
+          <h1>All assignment</h1>
+        </Row>
+        {assignments.map((item, index) => {
+          return (
+            <Link to={`./myAssignment/${item._id}`} style={{textDecoration:'none'}}>
+          <AssignmentCard title={item.title} dueDate={item.dueDate && item.dueDate.slice(0,15)} subject={item.subject}/>
+          </Link>
+          );
         })}
       </Container>
-            
-        </div>
+    </div>
     )
 }
 
