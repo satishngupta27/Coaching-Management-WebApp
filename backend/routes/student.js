b15f0bdcee38eaed3dae5c91f7f16580776b41e0
SATISH <satish.gupta_cs18@gla.ac.in>
2020-11-20 00:19:02 -0500

    login route

--- backend/routes/student.js
+++ backend/routes/student.js
@@ -39,6 +39,11 @@ router.route("/students").get((req, res) => {
   Student.find().then((students) => res.json(students));
 });
 
+router.route("/students/:id").get((req,res) =>{
+  const id = req.params.id;
+  Student.findById(id).then((student) => res.json(student));
+})
+
 router.route("/students/:id").put( async (req,res) => {
   const id  = req.params.id;
   const update = await Student.findByIdAndUpdate(id, req.body, {runValidators:true});
@@ -47,7 +52,15 @@ router.route("/students/:id").put( async (req,res) => {
 
 router.route("/students/:id").delete( async (req,res) =>{
   const id = req.params.id;
-  const deletestudent = await Student.findByIdAndDelete(id);
+  try{
+    const deletestudent = await Student.findByIdAndDelete(id);
+  }catch(err){
+    console.log(err)
+  }
+    
+    res.json(deletestudent)
+ 
+  
 })
 
 router.route("/countStudents").get(function(req, res) {
@@ -59,5 +72,43 @@ router.route("/countStudents").get(function(req, res) {
     }
   });
 });
+
+router.route('/studentLogin').post((req,res)=>{
+   const { email, password } = req.body;
+   console.table({ email, password });
+    Student.findOne({ email }).exec((err, user) => {
+      if (err || !user) {
+          return res.status(400).json({
+              error: 'User with that email does not exist. Please register.',
+              valid:false
+          });
+      }
+      if(password!=user.mobileNumber){
+        return res.status(400).json({
+          error:"password is not correct",
+          valid:false
+        })
+      }
+      
+      return res.status(200).json({
+        success: 'login success',
+        valid:true
+    });
+      // authenticate
+      // if (!user.authenticate(password)) {
+      //     return res.status(400).json({
+      //         error: 'Email and password do not match'
+      //     });
+      // }
+      // generate token and send to client
+      // const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+      // const { _id, name, email, role } = user;
+
+      // return res.json({
+      //     token,
+      //     user: { _id, name, email, role }
+      // });
+  });
+})
 
 module.exports = router;
