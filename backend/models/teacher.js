47a7c014530f4b857bb723ff92e630931d19cbae
SATISH <satish.gupta_cs18@gla.ac.in>
2020-11-20 12:22:21 -0500

    teacher model schema

--- backend/models/teacher.js
+++ backend/models/teacher.js
@@ -1,84 +1,19 @@
 const mongoose = require('mongoose');
-const crypto = require('crypto');
+const teacherSchema ={
+    firstName:String,
+    lastName: String,
+    email: String,
+    mobileNumber:String,
+    
+    gender:String,
+    birthDate:Date,
+    
+    address: String,
+    state:String,
+    city:String
 
-const teacherSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            trim: true,
-            required: true,
-            max: 12,
-            unique: true,
-            index: true,
-            lowercase: true
-        },
-        name: {
-            type: String,
-            trim: true,
-            required: true,
-            max: 32
-        },
-        email: {
-            type: String,
-            trim: true,
-            required: true,
-            unique: true,
-            lowercase: true
-        },
-        hashed_password: {
-            type: String,
-            required: true
-        },
-        salt: String,
-        role: {
-            type: String,
-            default: 'subscriber'
-        },
-        resetPasswordLink: {
-            data: String,
-            default: ''
-        }
-    },
-    { timestamps: true }
-);
+}
 
-// virtual fields
-userSchema
-    .virtual('password')
-    .set(function(password) {
-        // create temp variable called _password
-        this._password = password;
-        // generate salt
-        this.salt = this.makeSalt();
-        // encrypt password
-        this.hashed_password = this.encryptPassword(password);
-    })
-    .get(function() {
-        return this._password;
-    });
 
-// methods > authenticate, encryptPassword, makeSalt
-userSchema.methods = {
-    authenticate: function(plainText) {
-        return this.encryptPassword(plainText) === this.hashed_password;
-    },
-
-    encryptPassword: function(password) {
-        if (!password) return '';
-        try {
-            return crypto
-                .createHmac('sha1', this.salt)
-                .update(password)
-                .digest('hex');
-        } catch (err) {
-            return '';
-        }
-    },
-
-    makeSalt: function() {
-        return Math.round(new Date().valueOf() * Math.random()) + '';
-    }
-};
-// export user model
-
-module.exports = mongoose.model('teacher', userSchema);
+const Teacher = mongoose.model("Teacher",teacherSchema);
+module.exports=Teacher;
